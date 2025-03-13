fdce7ee76a98fa86c831916accc5873cb39d978a
unknown <aladynjr@gmail.com>
2025-03-13 20:46:24 +0100

    udpate firestore rules to allow project issue count update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -173,27 +173,26 @@ const Home = () => {
                       <h3 className="text-lg font-medium text-gray-900 group-hover:text-blue-600 transition-colors">{project.name}</h3>
                       
                       {/* Issue metrics */}
-                      {(project.totalIssues ?? 0) > 0 && (
+                     
                         <div className="flex items-center gap-3 mt-1 md:mt-0">
                           <span className="inline-flex items-center text-sm text-gray-500">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                               <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
                             </svg>
-                            {project.totalIssues ?? 0} questions
+                            {project.totalIssues ?? 0} issues
                           </span>
                           
-                          {(project.solvedIssues ?? 0) > 0 && (
                             <span className="inline-flex items-center text-sm">
-                              <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1 text-green-500" viewBox="0 0 20 20" fill="currentColor">
+                              <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 mr-1 text-gray-500" viewBox="0 0 20 20" fill="currentColor">
                                 <path fillRule="evenodd" d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                               </svg>
-                              <span className="text-green-600 font-medium">
+                              <span className="text-gray-600 font-medium">
                                 {getSolvedPercentage(project.totalIssues ?? 0, project.solvedIssues ?? 0)}% solved
                               </span>
                             </span>
-                          )}
+                          
                         </div>
-                      )}
+                   
                     </div>
                     <p className="text-gray-600 text-base mb-3 line-clamp-2">{project.description}</p>
                     
